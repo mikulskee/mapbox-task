916965ab7b737b0446eb0d80e8679f8e342b387d
Mateusz Mikulski <mikulskee@gmail.com>
2020-04-04 12:43:21 +0200

    initial commit - basic config

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./assets/css/bootstrap.min.css";
+import "./assets/css/now-ui-kit.min.css";
+import "./assets/demo/demo.css";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
