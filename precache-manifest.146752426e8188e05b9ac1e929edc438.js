42e349abe67b206dc1ab80605ed2c179727e62c2
Mateusz Mikulski <mikulskee@gmail.com>
2020-04-05 17:55:10 +0200

    Updates

---
+++ precache-manifest.146752426e8188e05b9ac1e929edc438.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a800f2b9612f7d935d8b32e3426e088",
+    "url": "/mapbox-task/index.html"
+  },
+  {
+    "revision": "224fe05c0be201f3de7b",
+    "url": "/mapbox-task/static/css/main.d1e39dbb.chunk.css"
+  },
+  {
+    "revision": "2632b5bb8d21008e9164",
+    "url": "/mapbox-task/static/js/2.d7e526b2.chunk.js"
+  },
+  {
+    "revision": "63a44dcd0ffba725ac4bd29695342440",
+    "url": "/mapbox-task/static/js/2.d7e526b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "224fe05c0be201f3de7b",
+    "url": "/mapbox-task/static/js/main.54b920fd.chunk.js"
+  },
+  {
+    "revision": "17921708a3906e1b7b4e",
+    "url": "/mapbox-task/static/js/runtime-main.6a443ef7.js"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/mapbox-task/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/mapbox-task/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/mapbox-task/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/mapbox-task/static/media/nucleo-outline.8ebec31f.woff2"
+  }
+]);
